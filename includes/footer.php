08104cfa35972606c247e74a3b7b987cb40869d2
Shaikh Tabrej Alam <shaikhtabrejalam96@gmail.com>
2025-01-02 23:44:45 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/" target="_blank"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://www.twitter.com/" target="_blank"><span>Twitter</span></a></li>
+				<li><a class="flickr" href="https://www.flicker.com/" target="_blank"><span>Flickr</span></a></li>
+				<li><a class="googleplus" href="https://www.google+.com/" target="_blank"><span>Google+</span></a></li>
+				<li><a class="dribbble" href="https://dribbble.com/" target="_blank"><span>Dribbble</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> TDC . All Rights Reserved </p>
+	</div>
+</div>
